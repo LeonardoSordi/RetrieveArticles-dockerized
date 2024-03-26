a10f35550fecb69d7ac18b8e5da29afd24c31c01
LeonardoSordi <115223267+me-sparo@users.noreply.github.com>
2024-03-26 15:03:35 +0100

    Author Key check on Create - OK

--- app/services/retrieved_articles/create_article_request.rb
+++ app/services/retrieved_articles/create_article_request.rb
@@ -2,6 +2,8 @@ require "http"
 class CreateArticleRequest
 
   def self.send_request(params)
+
+
     request = HTTP.post("http://api:3000/articles", form: params)
     request
   end
--- app/services/retrieved_articles/destroy_article_request.rb
+++ app/services/retrieved_articles/destroy_article_request.rb
@@ -6,31 +6,14 @@ class DestroyArticleRequest
   def self.send_request(article_id)
 
     article_request = HTTP.get("http://api:3000/articles/#{article_id}")
-
     parsed_article_request = JSON.parse(article_request.body)
-
     author_id = parsed_article_request["article"]["author_id"].to_s
 
-    puts "AUTHOR_ID: "
-    puts author_id
-
-
     author_request = HTTP.get("http://api:3000/authors/#{author_id}")
-
-    puts "RESPONSE INTO SERVICE: "
-    puts author_request
-    puts"response____end"
-
     parsed_author_request = JSON.parse(author_request.body)
-
     author_key = parsed_author_request["author"]["key"]
 
-    puts "KEY: "
-    puts author_key
-    puts"response____end"
-
     delete_article_request = HTTP.delete("http://api:3000/articles/#{article_id}", :params => {:author_key => author_key})
-
     delete_article_request
 
   end
